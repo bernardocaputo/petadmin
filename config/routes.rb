d66b96e56ad18d43dbc946b647d6f654487583ff
Bernardo Caputo <bernardocaputo@gmail.com>
2018-01-11 10:25:21 -0200

    create fae scaffold for all tables

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
 
   namespace :admin do
+    resources :sells
+    resources :products
+    resources :suppliers
+    resources :campaigns
+    resources :discounts
+    resources :services
+    resources :clients
   end
   # mount Fae below your admin namespec
   mount Fae::Engine => '/admin'
