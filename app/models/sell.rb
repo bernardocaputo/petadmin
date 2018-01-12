d09c996ead8d1c3b630398d2eeb53c1a9ca66681
Bernardo Caputo <bernardocaputo@gmail.com>
2018-01-12 15:04:22 -0200

    add datepicker for sell, create transaction many-to-many tables, adjust models with has_many and adjust sell form

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,9 +1,13 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
-  enum status: { finished: 0, canceled: 1 }
 
+  enum status: { finished: 0, canceled: 1 }
   validates :client, presence: true
-  
+  has_many :sell_products
+  has_many :products, through: :sell_products
+  has_many :sell_services
+  has_many :services, through: :sell_services
+
   def fae_display_field
     id
   end
